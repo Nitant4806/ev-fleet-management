959de83f0028ef97f6ee033cb779d5544e4281f8
Nitant <hedambanitant@gmail.com>
2026-06-19 21:56:49 +0530

    prepare render deployment

--- app/core/config.py
+++ app/core/config.py
@@ -2,14 +2,15 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+
     DB_USER: str
     DB_PASSWORD: str
     DB_HOST: str
     DB_PORT: str
     DB_NAME: str
 
-    REDIS_HOST: str
-    REDIS_PORT: int
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
 
     class Config:
         env_file = ".env"
